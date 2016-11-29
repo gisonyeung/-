ff0ea1ed6399be4fd98d55e3f24c6a85e2635fec
子聪 <gisonyeung2015@163.com>
2016-11-29 22:58:07 +0800

    add rsa

--- js/index.js
+++ js/index.js
@@ -676,7 +676,21 @@
 	}
 
 
+	/*
+		RSA
+	*/
+	function RSA() {
 
+		this.isPrime = function(num) {
+			for (var i = 2; i <= Math.sqrt(num); i++) {
+				if ( num % i == 0 ) {
+					return false;
+				}
+			};
+			return true;
+		}
+
+	}
 
 
 
